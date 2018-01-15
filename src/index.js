a4fb8d3e044aa9a8c65f1399df8adc9469b9b649
daiiz <daiz713@users.noreply.github.com>
2018-01-15 23:35:20 +0900

    Prototype (#1)
    * html-nodes-in-areaを真似て書いた
    
    * build
    
    * _isTheTopLayerの計算をaノードのchildNodeを用いて行う
    
    * add sample
    
    * dfsやめてquerySelectorAllでa要素を一気に取得する
    
    * rangeをwidth, heightでの指定も可能
    
    * maint
    
    * v1.0.1
    
    * resにwidth, heightを追加
    
    * v1.0.3
    
    * build
    
    * windowX, windowY を足す
    
    * anchorNodeでtopLayer判定を行ってみる
    
    * maint

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import $ from 'jquery'
+
+export default class AnchorsInArea {
+  constructor (node=window.document) {
+    this.root = node
+    this.options = {
+      excludeInvisibles: true,
+      onlyInTopLayer: true,
+      onlyHttpUrl: true,
+      maxDepth: 20
+    }
+    this.initialize()
+  }
+
+  initialize () {
+    this.range = {}
+    this.anchors = []
+  }
+
+  _isInvolvedIn ({ top, left, bottom, right }) {
+    if (!top || !left || !bottom || !right) return false
+    var X = this.range.left
+    var Y = this.range.top
+    var Xw = this.range.right || this.range.left + this.range.width
+    var Yh = this.range.bottom || this.range.top + this.range.height
+    if (X <= left && right <= Xw && Y <= top && bottom <= Yh) return true
+    return false
+  }
+
+  _isTheTopLayer (anchor, anchorNode) {
+    const points = [
+      [anchor.position.left + 1, anchor.position.top + 1],
+      [anchor.position.right - 1, anchor.position.top + 1],
+      [anchor.position.right - 1, anchor.position.bottom - 1],
+      [anchor.position.left + 1, anchor.position.bottom - 1]
+    ]
+
+    for (let point of points) {
+      const element = document.elementFromPoint(
+        point[0] - window.pageXOffset, point[1] - window.pageYOffset)
+      if (element === anchorNode || $(element).find(anchorNode).length > 0 ||
+        $(anchorNode).find(element).length > 0) return true
+    }
+    return false
+  }
+
+  find ({ top, left, bottom, right, width, height }) {
+    if (height) bottom = top + height
+    if (width) right = left + width
+    if (top === undefined || left === undefined
+      || bottom === undefined || right === undefined) return []
+    this.initialize()
+    top += window.scrollY
+    bottom += window.scrollY
+    left += window.scrollX
+    right += window.scrollX
+    this.range = { top, left, bottom, right }
+
+    // XXX: 候補をもう少し小さくできないか
+    const candidateAnchorNodes = document.querySelectorAll('a')
+
+    for (let anchorNode of candidateAnchorNodes) {
+      const href = anchorNode.href
+      if (this.options.onlyHttpUrl && !href.startsWith('http')) continue
+      const rect = anchorNode.getBoundingClientRect()
+      if (this.options.excludeInvisibles &&
+        rect.top === 0 && rect.bottom === 0 && rect.left === 0 && rect.right === 0) {
+        continue
+      }
+      const anchor = {
+        text: anchorNode.innerText.trim(),
+        url: anchorNode.href || '',
+        position: {
+          top: rect.top + window.scrollY,
+          left: rect.left + window.scrollX,
+          bottom: rect.bottom + window.scrollY,
+          right: rect.right + window.scrollX,
+          width: rect.right - rect.left,
+          height: rect.bottom - rect.top
+        }
+      }
+
+      if (!this._isInvolvedIn(anchor.position)) continue
+      if (this.options.onlyInTopLayer && !this._isTheTopLayer(anchor, anchorNode)) continue
+      this.anchors.push(anchor)
+    }
+
+    return this.anchors
+  }
+}
