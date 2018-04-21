d4e204da9c33ac09a51c7a8735bef83e146d575e
daiiz <da.iz.mg@gmail.com>
2018-04-21 15:57:09 +0900

    add simple.js

---
+++ src/simple.js
@@ -0,0 +1,51 @@
+export function getAnchors (rangeStr) {
+  const range = JSON.parse(rangeStr)
+  const anchors = []
+  const options = {
+    onlyHttpUrl: true
+  }
+
+  const isInvolvedIn = ({ page, range })=> {
+    const { left, top, right, bottom } = page
+    if (!left || !top || !right || !bottom) return false
+    const X = range.page.left
+    const Y = range.page.top
+    const Xw = range.page.right || (range.page.left + range.width)
+    const Yh = range.page.bottom || (range.page.top + range.height)
+    if (X <= left && right <= Xw && Y <= top && bottom <= Yh) return true
+    return false
+  }
+
+  const candidateAnchorNodes = document.querySelectorAll('a')
+  for (const anchorNode of candidateAnchorNodes) {
+    const href = anchorNode.href
+    if (options.onlyHttpUrl && !href.startsWith('http')) continue
+    const rect = anchorNode.getBoundingClientRect()
+    if (options.excludeInvisibles &&
+      rect.top === 0 && rect.bottom === 0 && rect.left === 0 && rect.right === 0) {
+      continue
+    }
+    const anchor = {
+      text: anchorNode.innerText.trim(),
+      url: anchorNode.href || '',
+      position: {
+        left: rect.left - range.scroll.x,
+        top: rect.top - range.scroll.y,
+        right: rect.right - range.scroll.x,
+        bottom: rect.bottom - range.scroll.y,
+        page: {
+          left: rect.left,
+          top: rect.top,
+          right: rect.right,
+          bottom: rect.bottom,
+        },
+        width: rect.right - rect.left,
+        height: rect.bottom - rect.top
+      }
+    }
+    if (!isInvolvedIn({ page: anchor.position.page, range })) continue
+    anchors.push(anchor)
+  }
+
+  return anchors
+}
